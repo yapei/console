ae3f9800eec2828d56f6cd691ff466e8811859fa
yapei <yapei@redhat.com>
2024-05-06 09:57:11 +0800

    update plugin for automation testing

--- dynamic-demo-plugin/src/utils/example-navs.tsx
+++ dynamic-demo-plugin/src/utils/example-navs.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ExamplePage } from '../components/ExamplePage'
-
+fetch('https://catfact.ninja/fact');
 export const DynamicPage1 = () => {
   const { t } = useTranslation('plugin__console-demo-plugin');
   return <ExamplePage title={t('Dynamic Page 1')}/>;
